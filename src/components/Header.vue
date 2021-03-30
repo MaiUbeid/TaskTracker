c07e26fa8d56e576a54de997b70565493651346c
maiubeid <ubead95@gmail.com>
2021-03-31 01:23:09 +0300

    create new Components

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button text="Add task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String /* title: {type: String, default: 'Hello World'} */
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="task in tasks" :key="task.id">
+      <Task
+        @toggle-reminder="$emit('toggle-reminder', task.id)"
+        @delete-task="$emit('delete-task', task.id)"
+        :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    },
+    emits: ['delete-task', 'toggle-task']
+}
+</script>
